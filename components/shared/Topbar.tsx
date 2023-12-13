6a7753e5bd08213c5cef9c9e15b4f79fb674594e
Dhruv Kapoor <111127495+dhruvkapoorx@users.noreply.github.com>
2023-12-13 21:52:29 +0530

    new deploy

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,13 +2,14 @@ import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
+import {logo,logo2} from "../../public/assets/index"
 
 function Topbar() {
   return (
     <nav className='topbar'>
       <Link href='/' className='flex items-center gap-4'>
-        <Image src='/logo.svg' alt='logo' width={28} height={28} />
-        <p className='text-heading3-bold text-light-1 max-xs:hidden'>Threads</p>
+        <Image src={logo2} alt='logo' width={28} height={28} />
+        <p className='text-heading3-bold text-light-1 max-xs:hidden'>ZenTweet</p>
       </Link>
 
       <div className='flex items-center gap-1'>
---
+++ public/assets/index.js
@@ -0,0 +1,4 @@
+import logo from "./logo1.png"
+import logo2 from "./logo4.svg"
+export {logo,
+logo2,}
